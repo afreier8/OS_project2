f84c849864364b011a4a941bdd0782e3a0710081
April <afreier@uccs.edu>
2017-04-02 20:30:13 -0600

    task 1 and task 2

---
+++ project-2_code/Product-Consumer.c
@@ -0,0 +1,125 @@
+//
+//  Product-Consumer.c
+//  
+//
+//  Created by April Freier on 4/2/17.
+//
+//
+
+//#include "Product-Consumer.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+#define BUFFER_SIZE 5
+
+int end;
+int i,j;
+char buffer[BUFFER_SIZE];
+pthread_mutex_t mutex;
+pthread_cond_t empty,full;
+
+typedef struct strThreadData{
+    int thread_num;
+    int par_count;
+} thdata;
+
+void Producer(FILE *fp){
+    
+    char s1;
+    
+    s1 = fgetc(fp);
+    while(1){
+        pthread_mutex_lock(&mutex);
+        buffer[i] = s1;
+        p = (i+1)%BUFFER_SIZE;
+        
+        if((i-1)%BUFFER_SIZE == j){
+            pthread_cond_signal(&empty);
+            //   signal empty
+        }
+        if(i == j){
+            //		producer sleep
+            pthread_cond_wait(&full, &mutex);
+            //		producer awaken
+        }
+        pthread_mutex_unlock(&mutex);
+        
+        s1=fgetc(fp);
+        if(feof(fp))
+        {
+            pthread_mutex_lock(&mutex);
+            end = 1;
+            
+            pthread_cond_signal(&empty);
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+    }
+}
+void Consumer(void *ptr)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex);
+        if(i == j)
+        {
+            if(end) break;
+            else
+            {
+                pthread_cond_wait(&empty, &mutex);
+                if(i == j && end) break;
+            }
+        }
+        printf("%c",buffer[j]);
+        j = (j+1)%BUFFER_SIZE;
+
+        if(i == (j-1)%BUFFER_SIZE)
+        {
+            pthread_cond_signal(&full);
+            //		printf("signal full\n");
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+    
+    pthread_exit(NULL);
+    
+}
+
+
+int main(int argc, char *argv[])
+{
+    // PTHREADS producer and consumer
+    pthread_t p, c;
+    FILE *fp;
+    
+    i = 0;
+    j = 0;
+    
+    // open messages.txt
+    if((fp=fopen("messages.txt", "r"))==NULL)
+    {
+        printf("ERROR: within string.txt!\n");
+        return -1;
+    }
+    
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init (&empty, NULL);
+    pthread_cond_init (&full, NULL);
+    
+    end = 0;
+    
+    // create thread 1 and 2
+    pthread_create (&p, NULL, (void *) &Producer, (FILE *) fp);
+    pthread_create (&c, NULL, (void *) &Consumer, NULL);
+    
+    pthread_join(p, NULL);
+    pthread_join(c, NULL);
+    
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&empty);
+    pthread_cond_destroy(&full);
+    
+    return 1;
+}
---
+++ project-2_code/Product-Consumer.h
@@ -0,0 +1,14 @@
+//
+//  Product-Consumer.h
+//  
+//
+//  Created by April Freier on 4/2/17.
+//
+//
+
+#ifndef Product_Consumer_h
+#define Product_Consumer_h
+
+#include <stdio.h>
+
+#endif /* Product_Consumer_h */
